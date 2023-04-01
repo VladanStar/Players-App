203c18af806e34b2467eb89276b3a51bfb6cb0f1
VladanStar <vladancupric@gmail.com>
2023-04-01 16:35:04 +0200

    Add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {AngularFireModule} from "@angular/fire/compat"
+import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from '../environments/environment';
 
-import {FormsModule} from "@angular/forms";
-
+import { FormsModule } from '@angular/forms';
+import { PlayerCreateComponent } from './player-create/player-create.component';
 
 import { PlayerTableComponent } from './player-table/player-table.component';
 
-
-
 @NgModule({
-  declarations: [
-    AppComponent,
-
-    PlayerTableComponent
-  ],
+  declarations: [AppComponent, PlayerCreateComponent, PlayerTableComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
