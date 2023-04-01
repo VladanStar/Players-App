bc80a09cf740aa4ade88111fd7e0e14cc19f1f91
VladanStar <vladancupric@gmail.com>
2023-04-01 15:21:52 +0200

    Add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayerCreateComponent } from './player-create/player-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerCreateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/player-create/player-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-create',
+  templateUrl: './player-create.component.html',
+  styleUrls: ['./player-create.component.css']
+})
+export class PlayerCreateComponent {
+
+}
