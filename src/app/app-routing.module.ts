08a4f938e5b8a04a999e908ad1c6f8f25fcfef83
VladanStar <vladancupric@gmail.com>
2023-04-02 19:00:54 +0200

    Add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { PlayerTableComponent } from './player-table/player-table.component';
 import { PlayerCreateComponent } from './player-create/player-create.component';
 import { PlayerEditComponent } from './player-edit/player-edit.component';
+import { PlayerDetailComponent } from './player-detail/player-detail.component';
 
 const routes: Routes = [
 {path:"", component:PlayerTableComponent},
 {path:"player/add",component:PlayerCreateComponent},
 {path: 'player/edit/:id', component:PlayerEditComponent},
+{path:"player/:id", component: PlayerDetailComponent},
 ];
 
 @NgModule({
--- src/app/player-detail/player-detail.component.ts
+++ src/app/player-detail/player-detail.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Player } from '../models/player';
+import { PlayerService } from '../services/player.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-player-detail',
   templateUrl: './player-detail.component.html',
   styleUrls: ['./player-detail.component.css']
 })
-export class PlayerDetailComponent {
+export class PlayerDetailComponent  implements OnInit{
 
+  id:any;
+
+ player:Player={}
+constructor(private playerServis:PlayerService, private route:ActivatedRoute,
+  private router:Router){}
+ngOnInit(): void {
+  this.id= this.route.snapshot.paramMap.get("id")
+  if(this.id){
+
+
+  this.playerServis.get(this.id).subscribe(p => {
+   this.player = p;
+   console.log(this.player);
+ });
+}
+
+}
 }
