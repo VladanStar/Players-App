8f662eb9ab0022cd5031ea3ed6319c2e70b93dcc
VladanStar <vladancupric@gmail.com>
2023-04-02 17:52:50 +0200

    Add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlayerTableComponent } from './player-table/player-table.component';
 import { PlayerCreateComponent } from './player-create/player-create.component';
+import { PlayerEditComponent } from './player-edit/player-edit.component';
 
 const routes: Routes = [
 {path:"", component:PlayerTableComponent},
